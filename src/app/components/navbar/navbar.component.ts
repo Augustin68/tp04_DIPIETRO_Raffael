df9544954130d4ad691b8d09e226b6eb43a089d1
Raffael Di Pietro <maxou4170@gmail.com>
2024-04-05 21:11:55 +0200

    feat(cart): creating a cart page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, TuiButtonModule],
+  imports: [CommonModule, TuiButtonModule, RouterLink],
   templateUrl: './navbar.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {}
